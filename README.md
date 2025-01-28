084027b459d2b1a1b45291d86f23803d7372efa0
Tausopy <122395389+Tausopy@users.noreply.github.com>
2025-01-28 09:15:58 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# tp4_temporaire_pd_cloud_whalla_je_le_supprime
+ cours de cloud, donc ntm
---
+++ app.py
@@ -0,0 +1,6 @@
+import uvicorn
+
+if __name__ == "__main__":
+	uvicorn.run("api:app", host="51.20.72.40", port=8000)
+
+
